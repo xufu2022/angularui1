4f06ac7661d4cbb970f476751a3de5bffe9d3606
Your Name <you@example.com>
2022-07-21 10:08:59 +0100

    t

--- angularui/src/app/app.module.ts
+++ angularui/src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppLayoutComponent } from './layout/app-layout/app-layout.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { AdminComponent } from './admin/admin.component';
+import { Page404Component } from './authentication/page404/page404.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { PageLoaderComponent } from './layout/page-loader/page-loader.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import {
+  PerfectScrollbarModule,
+  PERFECT_SCROLLBAR_CONFIG,
+  PerfectScrollbarConfigInterface,
+} from 'ngx-perfect-scrollbar';
 
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true,
+  wheelPropagation: false,
+};
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppLayoutComponent,
+    MainLayoutComponent,
+    AdminComponent,
+    Page404Component,
+    HeaderComponent,
+    PageLoaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    PerfectScrollbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
